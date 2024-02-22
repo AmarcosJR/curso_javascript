d0af1a7e86607f10a99f0a33a1285aa349be59aa
Marcos Junior <amarcos1337@outlook.com>
2024-02-22 15:19:27 -0300

    feat: create aula-01

---
+++ aula-01.js
@@ -0,0 +1,17 @@
+// exibe uma mensagem no console.
+// show a console message
+console.log('Teste');
+
+// entendendo melhor o uso do console
+// understanding the console usage
+const x = 1;
+const y = 3;
+
+// ao criar uma função e dar Run/Salvar não irá exibir nada, pois não esta sendo pedido para ser exibido via console
+// after create a function and run it, this will not be displayed, because we dont have a console.log function 
+const soma = x + y;
+
+// precisamos adicionar a função console.log para exibir o resultado
+// we need to add console.log function to display the result
+
+console.log('O resultado é: ', soma);
