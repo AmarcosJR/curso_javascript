9c508cacb828049728aa5c212bdb5417a1ec87fe
Marcos Junior <amarcos1337@outlook.com>
2024-02-23 14:49:24 -0300

    feat: aula-09

---
+++ aula-09.js
@@ -0,0 +1,16 @@
+// LEARNING ABOUT FUNCTIONS
+
+// function // name of function // parameters of function to use
+
+function sum (item1, item2) {
+    return item1 + item2;
+}
+
+function sub (item1, item2) {
+    return item1 - item2;
+}
+
+// functions only be displayed when we call them
+
+console.log('result:', sum(5,6));
+console.log('result:', sub(5,6));
