a752b5af3baaa64870fcb18cf1e806eac5d61088
Marcos Junior <amarcos1337@outlook.com>
2024-02-23 09:46:08 -0300

    feat: aula-04

--- aula-04.js
+++ aula-04.js
@@ -4,6 +4,8 @@ const quantity = 10;
 let sum = 1;
 
 // variable / condition / increment or decrement
+// while the condition evaluate to true, they will run the block of code
+// when the loop have finished, they will jump off the block
 
 for (let i = 0; i < quantity; i++) {
     console.log(i);
