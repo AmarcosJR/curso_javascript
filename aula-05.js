bd7eecfb0c850d5feb724b300d6c2429c564838c
Marcos Junior <amarcos1337@outlook.com>
2024-02-23 09:51:14 -0300

    feat: aula-05

---
+++ aula-05.js
@@ -0,0 +1,18 @@
+// understanding the WHILE
+
+let age = 1;
+
+
+// this code will run, but will do a infinite loop
+// PS: caution on that, we dont wanna infinite loop at all
+
+// while(age < 18) {
+//     console.log('Pass');
+// }
+
+
+//have similarities with FOR, but on majority of time we use FOR to do loops
+while(age < 18) {
+    console.log('Pass');
+    idade = idade + 1;
+}
